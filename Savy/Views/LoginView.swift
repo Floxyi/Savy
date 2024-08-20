ee6e7a5e82e30d4c6b0a504362c7e2f1fbe6b7a2
nxdxnx <nadineschatz@posteo.de>
2024-08-20 18:18:10 +0200

    2-created-login-page

---
+++ Savy/Views/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  Savy
+//
+//  Created by Nadine Schatz on 20.08.24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Login Screen")
+            .font(.largeTitle)
+            .padding()
+    }
+}
+
+#Preview {
+    LoginView()
+}
