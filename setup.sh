a59fdc1560e05bf29eba79a496d9f1af19c84db2
Florian Winkler <62253521+Floxyi@users.noreply.github.com>
2025-03-02 11:35:34 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# Define colors
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 RED='\033[0;31m'
@@ -15,7 +14,6 @@ if ! command -v brew >/dev/null 2>&1; then
     echo "${YELLOW}Homebrew is not installed. Installing Homebrew now...${RESET}"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-    # Verify Homebrew installation
     if ! command -v brew >/dev/null 2>&1; then
         echo "${RED}Homebrew installation failed. Please install it manually and rerun this script.${RESET}"
         exit 1
@@ -27,7 +25,6 @@ if ! command -v swiftformat >/dev/null 2>&1; then
     echo "${YELLOW}SwiftFormat is not installed. Installing it now...${RESET}"
     brew install swiftformat
 
-    # Verify SwiftFormat installation
     if ! command -v swiftformat >/dev/null 2>&1; then
         echo "${RED}SwiftFormat installation failed. Please install it manually and rerun this script.${RESET}"
         exit 1
